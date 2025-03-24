741706161d58be8dd719dddcb94930e39890e495
Steve Hull <hullstevenj@gmail.com>
2025-03-24 16:34:48 -0700

    Learning how to store form data in an array

--- routes/admin.js
+++ routes/admin.js
@@ -3,12 +3,16 @@ const express = require('express');
 const rootDir = require('../util/path');
 const router = express.Router();
 
+const products = [];
+
 router.get('/add-product', (req, res, next) => {
     res.sendFile(path.join(rootDir, 'views', 'add-product.html' ));
 });
 
 router.post('/add-product', (req, res, next) => {
+    console.log(req.body);
     res.redirect('/');
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
