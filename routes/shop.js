c58b3b7f59a82bbc4de84feb5f2b345d949f104e
Steve Hull <hullstevenj@gmail.com>
2025-03-24 21:15:07 -0700

    Modified shop.pug and app.js to use pug templating

--- routes/shop.js
+++ routes/shop.js
@@ -6,8 +6,9 @@ const adminData = require('./admin');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    console.log('shop.js', adminData.products);
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+    res.render('shop');
+    //console.log('shop.js', adminData.products);
+    //res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
 module.exports = router;
