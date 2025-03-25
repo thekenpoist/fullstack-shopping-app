6ce539a56ca2b5187ea8ba4213e6731bad0c3a47
Steve Hull <hullstevenj@gmail.com>
2025-03-25 11:57:34 -0700

    Ongoing modifications to shop.js and shop.pug as I learn templating

--- routes/shop.js
+++ routes/shop.js
@@ -6,9 +6,8 @@ const adminData = require('./admin');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.render('shop');
-    //console.log('shop.js', adminData.products);
-    //res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+    const products = adminData.products;
+    res.render('shop', {prods: products, docTitle: 'Shop'});
 });
 
 module.exports = router;
