85d6f320747fd744c999a14e67e113d6433c33b8
Steve Hull <hullstevenj@gmail.com>
2025-04-03 20:02:48 -0700

    Added getIndex function to shop.js controller

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,7 +1,5 @@
 const Product = require('../models/product');
 
-
-
 // Controller to retrieve and render the list of products
 exports.getProducts = (req, res, next) => {
     Product.fetchAll(products => {
@@ -14,3 +12,15 @@ exports.getProducts = (req, res, next) => {
         });
     });
 };
+
+exports.getIndex = (req, res, next) => {
+    Product.fetchAll(products => {
+        res.render('shop/product-list', {
+            prods: products,                 // Array of products passed to the view
+            pageTitle: 'Shop',              
+            hasProducts: products.length > 0, // Flag to conditionally render product list
+            activeShop: true,               // Used to highlight the active nav link
+            productCSS: true                // Flag for loading product-specific CSS
+        });
+    });
+}
