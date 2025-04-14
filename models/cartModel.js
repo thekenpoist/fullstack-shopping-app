391ce0509b00e9a2a9b207fa92d8bbab5e49e3c2
Steve Hull <hullstevenj@gmail.com>
2025-04-13 17:31:52 -0700

    Code class Cart to write new cart and/or updated cart to cart.json

--- models/cartModel.js
+++ models/cartModel.js
@@ -5,7 +5,7 @@ const p = path.join(__dirname, '..', 'data', 'cart.json');
 
 
 module.exports = class Cart{
-    static addProduct() {
+    static addProduct(id, productPrice) {
 
         fs.readFile(p, (err, fileContent) => {
             let cart = {products: [], totalPrice: 0};
@@ -13,7 +13,23 @@ module.exports = class Cart{
                 cart = JSON.parse(fileContent);
 
             }
-        })
+            const existingProductIndex = cart.products.toString.findIndex(prod => prod.id === id);
+            const existingProduct = cart.products[existingProductIndex];
+            let updatedProduct;
+            if (existingProduct) {
+                updatedProduct = { ...existingProduct };
+                updatedProduct.qty = updatedProduct.qty + 1;
+                cart.products = [...cart.products];
+                cart.products[existingProductIndex] = updatedProduct;
+            } else {
+                updatedProduct = {id: id, qty: 1};
+                cart.products = [...cart.products, updatedProduct];
+            }
+            cart.productPrice = cart.productPrice + productPrice;
+            fs.writeFile(p, JSON.stringify(cart), (err) => {
+                console.log(err);
+            })
+        });
 
     }
 
