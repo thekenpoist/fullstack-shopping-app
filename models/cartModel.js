e986541a24aa1aaf7405aa0c190379fb6fa2c521
Steve Hull <hullstevenj@gmail.com>
2025-04-14 15:15:37 -0700

    Refactor cartModel.js to use try/catch when initializing cart.json

--- models/cartModel.js
+++ models/cartModel.js
@@ -3,19 +3,29 @@ const path = require('path');
 
 const p = path.join(__dirname, '..', 'data', 'cart.json');
 
-
 module.exports = class Cart{
     static addProduct(id, productPrice) {
 
         fs.readFile(p, (err, fileContent) => {
-            let cart = {products: [], totalPrice: 0};
-            if (err) {
-                cart = JSON.parse(fileContent);
-
+            let cart = { products: [], totalPrice: 0 };
+            if (!err) {
+                try {
+                    const parsed = JSON.parse(fileContent);
+                    cart = {
+                        products: parsed.products || [],
+                        totalPrice: parsed.totalPrice || 0
+                    };
+                } catch (err) {
+                    cart = { products: [], totalPrice: 0 };
+                }
             }
-            const existingProductIndex = cart.products.findIndex(prod => prod.id === id);
+
+            const existingProductIndex = cart.products.findIndex(
+                prod => prod.id === id
+            );
             const existingProduct = cart.products[existingProductIndex];
             let updatedProduct;
+
             if (existingProduct) {
                 updatedProduct = { ...existingProduct };
                 updatedProduct.qty = updatedProduct.qty + 1;
@@ -25,12 +35,12 @@ module.exports = class Cart{
                 updatedProduct = {id: id, qty: 1};
                 cart.products = [...cart.products, updatedProduct];
             }
-            cart.productPrice = cart.productPrice + productPrice;
+            cart.totalPrice = cart.totalPrice + +productPrice;
             fs.writeFile(p, JSON.stringify(cart), (err) => {
-                console.log(err);
+                if (err) {
+                    console.log('Error writing file:', err);
+                }
             })
         });
-
     }
-
 };
