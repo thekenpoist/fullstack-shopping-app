1eea65114abd953220acc7aa0b31102d22145742
Steve Hull <hullstevenj@gmail.com>
2025-04-15 15:04:16 -0700

    Refactor postAddProduct function in adminController.js to pass null id to the product class constructor

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -16,7 +16,7 @@ exports.postAddProduct = (req, res, next) => {
     const price = req.body.price;
     const description = req.body.description;
 
-    const product = new Product(title, imageUrl, description, price);
+    const product = new Product(null, title, imageUrl, description, price);
     product.save();
     res.redirect('/');
 };
