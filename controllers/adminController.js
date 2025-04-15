863af28f8fa372d3cd0ef5068800071975b8ef96
Steve Hull <hullstevenj@gmail.com>
2025-04-14 20:43:47 -0700

    Add getEditProduct function in adminController.js

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -3,7 +3,7 @@ const Product = require('../models/productModel');
 
 // Controller to render the form for adding a new product
 exports.getAddProduct = (req, res, next) => {
-    res.render('admin/add-product', {
+    res.render('admin/edit-product', {
         pageTitle: 'Add Product' 
     });
 };
@@ -18,6 +18,18 @@ exports.postAddProduct = (req, res, next) => {
     const product = new Product(title, imageUrl, description, price);
     product.save();
     res.redirect('/');
+};
+
+// Controller to render the form for editing a product
+exports.getEditProduct = (req, res, next) => {
+    const editMode = req.query.edit;
+    if (!editMode) {
+        return res.redirect('/');
+    }
+    res.render('admin/edit-product', {
+        pageTitle: 'Add Product',
+        editing: editMode
+    });
 };
 
 exports.getProducts = (req, res, next) => {
