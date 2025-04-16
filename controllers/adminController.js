b06efc80a4c1f8ae8897c49e0afb96db9f7b62cb
Steve Hull <hullstevenj@gmail.com>
2025-04-16 14:46:52 -0700

    Add postDeleteProduct function in adminController.js

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -60,3 +60,9 @@ exports.getProducts = (req, res, next) => {
         });
     });
 };
+
+exports.postDeleteProduct = (req, res, next) => {
+    const prodId = req.body.productId;
+    Product.deleteProductById(prodId);
+    res.redirect('/admin/products');
+};
