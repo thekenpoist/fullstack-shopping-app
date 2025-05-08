9d25d09db6b5844dc423332612693a14bd37c5df
Steve Hull <hullstevenj@gmail.com>
2025-05-07 18:53:40 -0700

    Remove all code pertaining to reading and writing from and to JSON files

--- models/productModel.js
+++ models/productModel.js
@@ -1,29 +1,5 @@
-const fs = require('fs');
-const path = require('path');
+
 const Cart = require('./cartModel');
-
-// Construct the absolute path to the products.json file
-const p = path.join(__dirname, '..', 'data', 'products.json');
-
-// Helper function to read products from the JSON file
-// Accepts a callback function (cb) to return the products once read
-const getProductsFromFile = (cb) => {
-    fs.readFile(p, (err, fileContent) => {
-        if (err) {
-            // If file doesn't exist or can't be read, return an empty array
-            return cb([]);
-        }
-        try {
-            // Try to parse the file content as JSON and pass it to the callback
-            const products = JSON.parse(fileContent);
-            cb(products);
-        } catch (e) {
-            // If JSON is malformed, log the error and return an empty array
-            console.error('Invalid JSON, returning empty array.');
-            cb([]);
-        }
-    });
-};
 
 // Product model class
 module.exports = class Product {
@@ -37,49 +13,20 @@ module.exports = class Product {
 
     // Save the current product instance to the JSON file
     save() {
-        getProductsFromFile(products => {
-            if (this.id) {
-                const existingProductIndex = products.findIndex(prod => prod.id === this.id);
-                const updatedProducts = [...products];
-                updatedProducts[existingProductIndex] = this;
-                fs.writeFile(p, JSON.stringify(updatedProducts), err => {
-                    if (err) console.log(err);
-                });
-            } else {
-                this.id = Math.random().toString();
-                // Push the current product into the list of existing products
-                products.push(this);
-                // Overwrite the JSON file with the updated product list
-                fs.writeFile(p, JSON.stringify(products), err => {
-                    if (err) console.log(err);
-                });
-            }
-            
-        });
+        
     }
 
     static deleteProductById(id) {
-        getProductsFromFile(products => {
-            const product = products.find(prod => prod.id === id);
-            const updatedProducts = products.filter(prod => prod.id !== id);
-            fs.writeFile(p, JSON.stringify(updatedProducts), err => {
-                if (!err) {
-                    Cart.deleteProductFromCart(id, product.price);
-                }
-            });
-        });
+        
     }
 
     // Static method to fetch all saved products
     // Accepts a callback to return the list once loaded
     static fetchAll(cb) {
-        getProductsFromFile(cb);
+
     }
 
     static findById(id, cb) {
-        getProductsFromFile(products => {
-            const product = products.find(p => p.id === id);
-            cb(product);
-        });
+        
     }
 };
