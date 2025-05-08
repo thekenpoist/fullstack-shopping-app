8b21b2d4dec59e177a7b8ea647839b02303284c7
Steve Hull <hullstevenj@gmail.com>
2025-05-08 09:43:50 -0700

    Fix typo in findById in productModel

--- models/productModel.js
+++ models/productModel.js
@@ -28,6 +28,6 @@ module.exports = class Product {
     }
 
     static findById(id, cb) {
-        return db.execute('SELECT * FROM products WHERE producdts.id = ?', [id]);
+        return db.execute('SELECT * FROM products WHERE products.id = ?', [id]);
     }
 };
