9d2ac81087eadb816ff655f8c15221b98ef7e441
Steve Hull <hullstevenj@gmail.com>
2025-05-07 19:12:26 -0700

    Refactor getIndex in shopController for SQL query

--- controllers/shopController.js
+++ controllers/shopController.js
@@ -23,12 +23,14 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-    Product.fetchAll(products => {
-        res.render('shop/index', {
-            prods: products,                 // Array of products passed to the view
-            pageTitle: 'Index',
-        });
-    });
+    Product.fetchAll()
+        .then(([rows, fieldData]) => {
+            res.render('shop/index', {
+                prods: products,                 // Array of products passed to the view
+                pageTitle: 'Index'
+            });
+        })     
+        .catch(err => console.log(err));
 };
 
 exports.getCart = (req, res, next) => {
