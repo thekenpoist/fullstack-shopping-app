6b162e23a48b47d13a3f486ddaa3e2f9e61fb74a
Steve Hull <hullstevenj@gmail.com>
2025-05-09 16:09:38 -0700

    Refactor database.js

--- config/database.js
+++ config/database.js
@@ -1,16 +1,12 @@
 require('dotenv').config();
 const Sequelize = require('sequelize');
 
-const Sequelize = new Sequelize()
+const Sequelize = new Sequelize(
+    process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+        process.env.DB_DIALECT, 
+        process.env.DB_HOST
+    });
 
 
-const mysql = require('mysql2');
-
-const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-});
 
 module.exports = pool.promise();
