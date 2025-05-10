6c80a5585ccfb95f74d3a4f697a103ebadc97aa2
Steve Hull <hullstevenj@gmail.com>
2025-05-10 08:16:53 -0700

    Refactor optional keys in sequelize in database.js

--- config/database.js
+++ config/database.js
@@ -3,8 +3,8 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(
     process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, { 
-        process.env.DB_DIALECT, 
-        process.env.DB_HOST
+        dialect: process.env.DB_DIALECT, 
+        host: process.env.DB_HOST
     });
 
 module.exports = sequelize;
