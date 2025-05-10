73f3e8bc1da37f5bfbff48f91d726581f3cb0d6c
Steve Hull <hullstevenj@gmail.com>
2025-05-10 08:33:11 -0700

    Refactor Product in productModel for sequelize

--- models/productModel.js
+++ models/productModel.js
@@ -7,5 +7,23 @@ const Product = sequelize.define('product', {
         autoIncrement: true,
         allowNull: false,
         primaryKey: true
+    },
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    price: {
+        type: Sequelize.DOUBLE,
+        allowNull: false
+    },
+    imageUrl: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    description: {
+        type: Sequelize.STRING,
+        allowNull: false
     }
 });
+
+module.exports = Product;
