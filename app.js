59f98d1fab5c1a15946f48a4cd236a61a8bbd563
Steve Hull <hullstevenj@gmail.com>
2025-06-11 21:30:22 -0700

    Import cartModel and cartItemModel to app.js and establish belongs to and belongs to many relationships between models in app.js

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const errorController = require('./controllers/errorController');
 const sequelize = require('./config/database');
 const Product = require('./models/productModel');
 const User = require('./models/userModel');
+const Cart = require('./models/cartModel');
+const CartItem = require('./models/cartItemModel');
 
 const app = express();
 
@@ -54,9 +56,13 @@ app.use(errorController.get404);
 
 Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'});
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
-sequelize.sync()
-//sequelize.sync({ force: true })
+//sequelize.sync()
+sequelize.sync({ force: true })
     .then(result => {
         return User.findByPk(1);
     })
